6f5575ec79c00e019ecd053fd522497f0dc72bad
Leo <zhili.c.w@gmail.com>
2023-08-09 17:53:37 +0800

    feat(module): nacos config loader
    clean src file

---
+++ l10nlite-loader/l10nlite-loader-nacos/src/main/java/com/zliio/l10nlite/loader/NacosL10nConfigurerLoader.java
@@ -0,0 +1,21 @@
+package com.zliio.l10nlite.loader;
+
+import com.zliio.l10nlite.core.L10nConfigurerLoader;
+import com.zliio.l10nlite.core.L10nValue;
+
+import java.util.List;
+
+/**
+ * ResourcesL10nConfigurerLoader.
+ * {scenario}.properties.
+ *
+ * @author Leo
+ * @since 1.0 (2023-07-26)
+ **/
+public class NacosL10nConfigurerLoader implements L10nConfigurerLoader {
+
+    @Override
+    public List<L10nValue> getL10nValues() {
+        return null;
+    }
+}
